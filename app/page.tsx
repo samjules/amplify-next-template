58bf60095f55027abb5791e506bc7d36ad0c3a54
samjules <samjulander@gmail.com>
2025-02-16 11:46:33 -0900

    Added use client to page and todo file, removed logic for todos from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+
+
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Tabs } from '@aws-amplify/ui-react';
 import Todos from '../app/Todos';
 
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  return (
     
 
+export default function home(){
+
+  return(
     <div>
 
   <Tabs
@@ -57,7 +32,10 @@ export default function App() {
 
     </div>
 
-    
-    
   );
+
 }
+    
+    
+    
+ 
