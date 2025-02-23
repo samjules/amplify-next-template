8d12e93d8b621555c4c72a001afecd78e25a24a1
samjules <samjulander@gmail.com>
2025-02-22 23:33:45 -0900

    added profile picture root to storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,6 +1,15 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: "aircraft-images",
-  // Amplify automatically handles access control via the backend configuration
-});
+    name: 'amplifyTeamDrive',
+    access: (allow) => ({
+      'profile-pictures/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'picture-submissions/*': [
+        allow.authenticated.to(['read','write']),
+        allow.guest.to(['read', 'write'])
+      ],
+    })
+  });
